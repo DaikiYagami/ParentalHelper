cd70ce04b66131c53e1fcff0ee1c557f1e6ff798
TomasuHD <regindemonsp@gmail.com>
2021-10-30 11:20:34 -0300

    Splashscreen, Login y Registro

---
+++ ParentalHelper/app/src/main/java/com/tomaspev/parentalhelper/Registro.kt
@@ -0,0 +1,13 @@
+package com.tomaspev.parentalhelper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Registro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+
+        supportActionBar?.hide()
+    }
+}
