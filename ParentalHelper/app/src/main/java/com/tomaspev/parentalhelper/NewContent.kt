afba3da7365d464a8c7e7fe351c3b3dbf58bfbd1
DaikiYagami <goliat.davidgarcia@gmail.com>
2021-10-30 11:26:08 -0300

    primeros pasos de new content

---
+++ ParentalHelper/app/src/main/java/com/tomaspev/parentalhelper/NewContent.kt
@@ -0,0 +1,11 @@
+package com.tomaspev.parentalhelper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewContent : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_content)
+    }
+}
