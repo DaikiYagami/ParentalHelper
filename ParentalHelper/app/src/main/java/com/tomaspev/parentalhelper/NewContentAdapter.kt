858d07824059226234f84e310ade99a72bf243e5
DaikiYagami <goliat.davidgarcia@gmail.com>
2021-10-31 01:21:00 -0300

    update con error

--- ParentalHelper/app/src/main/java/com/tomaspev/parentalhelper/NewContentAdapter.kt
+++ ParentalHelper/app/src/main/java/com/tomaspev/parentalhelper/NewContentAdapter.kt
@@ -6,12 +6,12 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.elemento_newcontent.view.*
 
-class NewContentAdapter            (private val mContext: Context, private val newContent: List<Contenido>) :
-    ArrayAdapter<Contenido>(mContext,0,newContent)
+class NewContentAdapter            (private val mContext: Context, private val detalle: List<Contenido>) :
+    ArrayAdapter<Contenido>(mContext,0,detalle)
 {
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val layout = LayoutInflater.from(mContext).inflate(R.layout.elemento_newcontent, parent, false)
-        val Contenido = newContent[position]
+        val Contenido = detalle[position]
         layout.titlenewcontent.text = Contenido.titulo
         layout.descriptionnewcontent.text = Contenido.descripcionShort
         return layout
