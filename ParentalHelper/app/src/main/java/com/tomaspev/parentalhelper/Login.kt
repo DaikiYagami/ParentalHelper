191935f2f0752ef8c4b56e6a9329e3e8a1c5e5c5
TomasuHD <regindemonsp@gmail.com>
2021-11-02 21:08:05 -0300

    02 - 11 - 2021

--- ParentalHelper/app/src/main/java/com/tomaspev/parentalhelper/Login.kt
+++ ParentalHelper/app/src/main/java/com/tomaspev/parentalhelper/Login.kt
@@ -4,15 +4,56 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_registro_usuario.*
+import kotlinx.android.synthetic.main.activity_registro_usuario.emailEditText
+import kotlinx.android.synthetic.main.activity_registro_usuario.passwordEditText
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         supportActionBar?.hide()
+
+        setup()
+        session()
     }
     fun registrarse(view: View){
         val intent = Intent(this, RegistroUsuario::class.java)
         startActivity(intent)
+    }
+    private fun setup(){
+        LogInButton.setOnClickListener  {
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()){
+
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(emailEditText.text.toString(),
+                    passwordEditText.text.toString()).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                    } else {
+                        showAlert()
+                    }
+                }
+            }
+        }
+    }
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error.")
+        builder.setPositiveButton("Aceptar",null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showHome(email: String, provider: ProviderType) {
+
+        val home = Intent(this, MainActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(home)
     }
 }
--- ParentalHelper/app/src/main/java/com/tomaspev/parentalhelper/RegistroUsuario.kt
+++ ParentalHelper/app/src/main/java/com/tomaspev/parentalhelper/RegistroUsuario.kt
@@ -1,7 +1,11 @@
 package com.tomaspev.parentalhelper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_registro_usuario.*
 
 class RegistroUsuario : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +13,40 @@ class RegistroUsuario : AppCompatActivity() {
         setContentView(R.layout.activity_registro_usuario)
 
         supportActionBar?.hide()
+        setup()
+    }
+
+    private fun setup(){
+        signUpButton.setOnClickListener {
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()){
+
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(emailEditText.text.toString(),
+                    passwordEditText.text.toString()).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                    } else {
+                        showAlert()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error.")
+        builder.setPositiveButton("Aceptar",null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showHome(email: String, provider: ProviderType) {
+
+        val home = Intent(this, MainActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(home)
     }
 }
