96c48fec52d4ff7dc86e503e8b3c090391cf7db7
Seba <sebastian1377@gmail.com>
2021-11-09 20:56:07 -0300

    Prueba video yutu 2

--- ParentalHelper/app/src/main/java/com/tomaspev/parentalhelper/RegistroAdapter.kt
+++ ParentalHelper/app/src/main/java/com/tomaspev/parentalhelper/RegistroAdapter.kt
@@ -41,7 +41,7 @@ class RegistroAdapter(var context: Context): RecyclerView.Adapter<RegistroAdapte
         val data = dataList[position]
 
         holder.nombre.text = data.nombre
-        holder.edad.text = daysBetween(data.cumple, todaysDate()).toString()
+        holder.edad.text = edad(data.cumple, fechaHoy()).toString()
         holder.genero.text = data.genero
         //holder.cumple.text = formatoFecha(data.cumple, "dd/MM")
         //holder.capacidad.setImageResource()
@@ -52,59 +52,4 @@ class RegistroAdapter(var context: Context): RecyclerView.Adapter<RegistroAdapte
     }
 
     override fun getItemCount() = dataList.size
-
-    // Para el formato de fecha que se necesita
-    private fun formatoFecha(fecha: String, output: String): String {
-        val formattedDate: String
-        when (output) {
-            "dd/MM" -> {
-                val parser = SimpleDateFormat("dd/MM/yyyy")
-                val formatter = SimpleDateFormat("dd/MM")
-                formattedDate = formatter.format(parser.parse(fecha))
-            }
-            "dd" -> {
-                val parser = SimpleDateFormat("dd/MM/yyyy")
-                val formatter = SimpleDateFormat("dd")
-                formattedDate = formatter.format(parser.parse(fecha))
-            }
-            "MM" -> {
-                val parser = SimpleDateFormat("dd/MM/yyyy")
-                val formatter = SimpleDateFormat("MM")
-                formattedDate = formatter.format(parser.parse(fecha))
-            }
-            "yyyy" -> {
-                val parser = SimpleDateFormat("dd/MM/yyyy")
-                val formatter = SimpleDateFormat("yyyy")
-                formattedDate = formatter.format(parser.parse(fecha))
-            }
-            else -> {
-                formattedDate = fecha
-            }
-        }
-        return formattedDate
-    }
-    // Devuelve la fecha de hoy
-    private fun todaysDate():String {
-        val cal = Calendar.getInstance()
-        val dd = cal.get(Calendar.DAY_OF_MONTH)
-        val mm = cal.get(Calendar.MONTH) + 1
-        val yy = cal.get(Calendar.YEAR)
-        return "$dd/$mm/$yy"
-    }
-    // Devuelve la cantidad de dias entre dos fechas
-    private fun daysBetween(fecha1: String, fecha2: String): Int {
-        val dd1 = formatoFecha(fecha1, "dd")
-        val dd2 = formatoFecha(fecha2, "dd")
-        val mm1 = formatoFecha(fecha1, "MM")
-        val mm2 = formatoFecha(fecha2, "MM")
-        val yy1 = formatoFecha(fecha1,"yyyy")
-        val yy2 = formatoFecha(fecha2,"yyyy")
-
-        var diff = yy2.toInt() - yy1.toInt()
-
-        if (mm2.toInt() < mm1.toInt() || (mm2.toInt() == mm1.toInt() && dd2.toInt() < dd1.toInt())) {
-            diff--
-        }
-        return diff
-    }
 }
