a2ad6663ec9c04a272a0f705c402b2ebfa17889a
TomasuHD <regindemonsp@gmail.com>
2021-11-09 23:16:45 -0300

    Cambios

--- ParentalHelper/app/src/main/java/com/tomaspev/parentalhelper/RegistroUsuario.kt
+++ ParentalHelper/app/src/main/java/com/tomaspev/parentalhelper/RegistroUsuario.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Patterns
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.text.trimmedLength
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_registro_usuario.*
 
@@ -25,13 +26,17 @@ class RegistroUsuario : AppCompatActivity() {
             } else if (!Patterns.EMAIL_ADDRESS.matcher(emailEditText.text).matches()){
                 Toast.makeText(this, "Introduzca un E-mail valido", Toast.LENGTH_SHORT).show()
             } else if (passwordEditText.text.isEmpty()) {
-                Toast.makeText(this, "Contraseña no puede estar vacia", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "La Contraseña no puede estar vacia", Toast.LENGTH_SHORT).show()
+            } else if (passwordEditText.text.length <6) {
+                Toast.makeText(this, "La contraseña debe poseer más de 6 caracteres", Toast.LENGTH_SHORT).show()
             } else if (emailEditText.text.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(emailEditText.text).matches() &&
                     passwordEditText.text.isNotEmpty()){
                     FirebaseAuth.getInstance().createUserWithEmailAndPassword(emailEditText.text.toString(),
                         passwordEditText.text.toString()).addOnCompleteListener {
                         if (it.isSuccessful) {
                             showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                    } else {
+                        showAlert()
                     }
                 }
             }
@@ -41,8 +46,8 @@ class RegistroUsuario : AppCompatActivity() {
     private fun showAlert() {
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Error")
-        builder.setMessage("Se ha producido un error.")
-        builder.setPositiveButton("Aceptar",null)
+        builder.setMessage("Este E-Mail ya esta en uso")
+        builder.setPositiveButton("Ok",null)
         val dialog: AlertDialog = builder.create()
         dialog.show()
     }
